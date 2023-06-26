a63fec730f6cc36158d8342dfc030fba8098c657
LawrenceGibbons <lkg5@cornell.edu>
2023-06-26 16:47:42 -0400

    More wire vs reg updates

--- hdl/adc_acq_top_CBUF.v
+++ hdl/adc_acq_top_CBUF.v
@@ -42,11 +42,11 @@ module adc_acq_top_cbuf(
 
 wire [1:0] fill_type;           // to determine how much data to collect
 wire [22:0] burst_start_adr;// first DDR3 burst memory location for this fill (3 LSBs = 0)
-reg [25:0] circ_buf_wr_dat;   // data to write to the circular buffer
-reg [15:0] circ_buf_rd_addr;   // address to read from the circular buffer
-reg [15:0] circ_buf_trig_addr;   // circular buffer address corresponding to a trigger, FIFO output
+wire [25:0] circ_buf_wr_dat;   // data to write to the circular buffer
 wire [15:0] circ_buf_wr_addr;    // address to write to the circular buffer
 wire [25:0] circ_buf_rd_dat;     // data read from the circular buffer
+wire [15:0] circ_buf_rd_addr;   // address to read from the circular buffer
+wire [15:0] circ_buf_trig_addr;   // circular buffer address corresponding to a trigger, FIFO output
 wire [11:0] current_waveform_num;// the current waveform number, to be used in header
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
